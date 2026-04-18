a149aa899e293156a1ec690836b7026052b2b434
Manzar Shikhaliyeva <mshikhal@my.centennialcollege.ca>
2026-04-18 13:13:44 -0400

    Initial commit

---
+++ StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
